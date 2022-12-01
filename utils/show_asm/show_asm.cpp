f199cd6df42d870d22535d6d3ca6f11f36bec0ac
Shinnenijou <littlesword111@gmail.com>
2022-12-01 17:53:40 +0800

    update

--- utils/show_asm/show_asm.cpp
+++ utils/show_asm/show_asm.cpp
@@ -1,8 +1,7 @@
 // 该脚本用于将src.cpp代码编译为汇编语言
-// 该脚本编译后已复制到/usr/bin，可以在终端直接调用
 // 本脚本需要输入一个或两个参数
 // 第一个参数为可选参数，指定编译的优化等级，默认为-Og
-// 第二个参数为需要编译的文件
+// 第二个参数为需要编译的文件, 默认为src_to_show_asm, 可以将源码直接复制进对应文件进行编译
 #include<string>
 #include<cstdlib>
 
@@ -13,9 +12,15 @@ std::string get_name(char *s){
 }
 
 int main(int argc, char **argv){
-    if(argc < 2 || argc > 3) return -1;
+    if(argc > 3) return -1;
     std::string clang("clang -c -std=c++17 ");
-    std::string bifile_name(get_name(argv[argc - 1]) + ".o");
+    std::string bifile_name;
+    if (argc < 2){
+        bifile_name = std::string("src_to_show_asm");
+    }
+    else{
+        bifile_name = get_name(argv[argc - 1]) + ".o";
+    }
     std::string objdump("objdump -d " + bifile_name);
     std::string del("rm " + bifile_name);
     std::string O(argc>2?argv[argc-2]:"-Og");
