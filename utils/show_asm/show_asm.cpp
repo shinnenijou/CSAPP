a3198d25fb4403987fa62926241def518977880f
Shinnenijou <littlesword111@gmail.com>
2022-12-01 18:11:01 +0800

    update

--- utils/show_asm/show_asm.cpp
+++ utils/show_asm/show_asm.cpp
@@ -4,27 +4,30 @@
 // 第二个参数为需要编译的文件, 默认为src_to_show_asm, 可以将源码直接复制进对应文件进行编译
 #include<string>
 #include<cstdlib>
+#include<iostream>
 
-std::string get_name(char *s){
+std::string get_name(std::string &s){
     int i;
-    for(i = 0; s[i] != '.' && s[i] != '\0'; ++i);
-    return std::string(s, i);
+    for(i = 0; s[i] != '.' && i != s.size(); ++i);
+    return std::string(s.c_str(), i);
 }
 
 int main(int argc, char **argv){
     if(argc > 3) return -1;
+
+    std::string filename = std::string("src_to_show_asm.cpp");
     std::string clang("clang -c -std=c++17 ");
-    std::string bifile_name;
-    if (argc < 2){
-        bifile_name = std::string("src_to_show_asm");
+
+    if (argc >= 2){
+        filename = std::string(argv[argc - 1]);
     }
-    else{
-        bifile_name = get_name(argv[argc - 1]) + ".o";
-    }
+
+    std::string bifile_name(get_name(filename) + ".o");
+
     std::string objdump("objdump -d " + bifile_name);
     std::string del("rm " + bifile_name);
     std::string O(argc>2?argv[argc-2]:"-Og");
-    clang += O + " " + std::string(argv[argc-1]);
+    clang += O + " " + filename;
     system(clang.c_str());
     system(objdump.c_str());
     system(del.c_str());
