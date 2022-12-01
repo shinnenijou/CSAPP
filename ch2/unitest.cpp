d4eabf8948d5cc41cbed916e9e0e8f95662472eb
Shinnenijou <littlesword111@gmail.com>
2022-12-01 21:33:10 +0800

    unit test file

--- ch2/unitest.cpp
+++
@@ -1,7 +0,0 @@
-#include "ch2_hw.cpp"
-#include "../utils/common.h"
-#include<iostream>
-
-int main(){
-    return 0;
-}
---
+++ unitest.cpp
@@ -0,0 +1,10 @@
+#include "ch2/ch2_hw.cpp"
+#include "utils/common.h"
+#include<iostream>
+
+int main(){
+    int i = 0;
+    i++;
+    std::cout << i << std::endl;
+    return 0;
+}
