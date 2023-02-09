c7b36fa100494e8f4fb7d1fd64af29d09e044419
shinnenijou <littlesword111@gmail.com>
2023-02-09 21:13:31 +0800

    update
    好起来了

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include "common/catch2/catch_amalgamated.hpp"
 #include "common/rio/rio.h"
 
-int test(){
-    return 3;
+int test(int num){
+    return num;
 }
 
 TEST_CASE("Unit Test", "[Test]"){
-    REQUIRE ( test() == 3 );
-    REQUIRE ( test() == 3 );
+    REQUIRE ( test(3) == 3 );
+    REQUIRE ( test(3) == 3 );
 }
