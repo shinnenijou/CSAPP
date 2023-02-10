282ca57b6416f6cfcbed71932cbec8808563e7eb
shinnenijou <littlesword111@gmail.com>
2023-02-10 18:58:19 +0800

    update
    源文件头文件就该乱七八糟混在一个文件夹（大声

--- common/common.h
+++ common/common.h
@@ -1,8 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include "./network/network.h"
-#include "./rio/rio.h"
-#include "./wrapper/unix_wrap.h"
+#include "network.h"
+#include "rio.h"
+#include "unix_wrap.h"
 
 #endif
