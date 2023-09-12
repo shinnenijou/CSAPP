08a09717dace69773df38287b32be7a712a19a03
Shinnenijou <52779368+shinnenijou@users.noreply.github.com>
2023-09-13 02:00:37 +0800

    update (#1)

--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -1,21 +1,12 @@
 #include "catch_amalgamated.hpp"
 #include "catch_amalgamated.cpp"
 
+
 int fits_bits(int x, int n);
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( fits_bits( 0x78, 1) == 0 );
-    REQUIRE( fits_bits( 0x78, 7) == 0 );
-    REQUIRE( fits_bits( 0x78, 8) == 1 );
-    REQUIRE( fits_bits( 0x78, 9) == 1 );
-    REQUIRE( fits_bits( 0xFF, 1) == 0 );
-    REQUIRE( fits_bits( 0xFF, 7) == 0 );
-    REQUIRE( fits_bits( 0xFF, 8) == 0 );
-    REQUIRE( fits_bits( 0xFF, 9) == 1 );
-    REQUIRE( fits_bits( 0xFFFFFFFA, 1) == 0 );
-    REQUIRE( fits_bits( 0xFFFFFFFA, 2) == 0 );
-    REQUIRE( fits_bits( 0xFFFFFFFA, 3) == 0 );
-    REQUIRE( fits_bits( 0xFFFFFFFA, 4) == 1 );
-    REQUIRE( fits_bits( 0xFFFFFFFA, 5) == 1 );
-
+    REQUIRE( saturating_add( 0, 0) == 0 );
+    REQUIRE( saturating_add( 0x80000000, 0x80FFFFFF) == 1 );
+    REQUIRE( saturating_add( 0x80000000, 0x7FFFFFFF) == 0 );
+    REQUIRE( saturating_add( 0x7FFFFFFF, 0x7FFFFFFF) == 1 );
 }
