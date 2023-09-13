3e2a55e53ae58d211d248e89c61846a62b21cef2
Shinnenijou <littlesword111@gmail.com>
2023-09-13 17:17:53 +0800

    update

--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -1,12 +1,6 @@
 #include "catch_amalgamated.hpp"
 #include "catch_amalgamated.cpp"
 
-
-int fits_bits(int x, int n);
-
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( saturating_add( 0, 0) == 0 );
-    REQUIRE( saturating_add( 0x80000000, 0x80FFFFFF) == 1 );
-    REQUIRE( saturating_add( 0x80000000, 0x7FFFFFFF) == 0 );
-    REQUIRE( saturating_add( 0x7FFFFFFF, 0x7FFFFFFF) == 1 );
+
 }
